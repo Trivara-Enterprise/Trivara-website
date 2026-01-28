53b47b13af22a6a58ef5221f98588410d54c03ae
Balamurugan1962 <Balamurugan.sekar@icloud.com>
2026-01-28 09:52:19 +0530

    chore: nextconfig for posyhog

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  /* config options here */
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
